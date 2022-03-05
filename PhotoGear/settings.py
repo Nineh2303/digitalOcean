389296b7e7302930428c014a7f3589c45a0304df
Nguyen Duc Chinh <nineh2303@gmail.com>
2022-03-05 14:32:23 +0700

    change-5

--- PhotoGear/settings.py
+++ PhotoGear/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -163,6 +164,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static_in_env')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
